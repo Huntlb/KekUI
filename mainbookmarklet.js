71c46eb92eda1b3f7859e8e642f2d8229c998347
Huntlb <100316201+Huntlb@users.noreply.github.com>
2024-01-15 15:14:49 -0800

    Create mainbookmarklet.js

---
+++ mainbookmarklet.js
@@ -0,0 +1,5 @@
+javascript:(function() {
+  var script = document.createElement('script');
+  script.src = 'https://raw.githubusercontent.com/Huntlb/KekUI/main/main.js';
+  document.body.appendChild(script);
+})();
